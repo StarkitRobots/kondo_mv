b2f4dbe86a1413d75ec0c237edbe56fd97a086d4
Ilya <ris2012.r@yandex.ru>
2019-11-01 20:41:07 +0300

    loc

--- localization/localization.py
+++ localization/localization.py
@@ -4,7 +4,8 @@ class Localization:
     def __init__(self):
         self.ball_position = None
         self.robot_position = None
-
+        self.localized = False
+        self.seeBall = False
     def update(self, data):
         self.ball_position = data["ball"]
         self.robot_position = updatePF(data)
