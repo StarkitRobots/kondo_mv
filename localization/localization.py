b51f8b7388f4fafe1e6a6b56fc267939674307ce
Ilya <ris2012.r@yandex.ru>
2019-11-04 15:43:00 +0300

    i fixed some bad things with my folders

--- localization/localization.py
+++ localization/localization.py
@@ -7,16 +7,15 @@ class Localization:
         self.localized = False
         self.seeBall = False
         self.robot = Robot()
-        self.robot.set_coord(0.0, 0.0, 1.57)
+        self.robot.set_coord(0.0, 0.0, 0.0)
         with open("localization/landmarks.json", "r") as f:
                 landmarks = json.loads(f.read())
-        self.pf = ParticleFilter(self.robot, Field("localization/parfield.json"), landmarks, sense_noise=0.2)
+        self.pf = ParticleFilter(self.robot, Field("localization/parfield.json"), landmarks, sense_noise=1.0)
 
     def update(self, data):
         #self.ball_position = data["ball"]
-        self.robot_position = updatePF(self.pf, data)
+        self.robot_position = updatePF(self.pf, self.robot, data)
         return 0
 
-    def move(self, x, y, yaw):
-        self.pf.move(x,y, yaw)
-
+    def end_of_loc(self):
+        self.pf.logs.close()
