b657d0e68f99dceaf8d95b5aa2049b7a16f20ac6
Ilya <ris2012.r@yandex.ru>
2019-11-05 21:38:15 +0300

    motion.py fix

--- motion/motion.py
+++ motion/motion.py
@@ -4,27 +4,29 @@ from kondo_controller import Rcb4BaseLib
 from pyb import UART
 import json, math
 import time
-from motions import *
+sys.path.append('motion')
+from motion import *
 
 class Motion:
     def __init__(self):
         # RCB4 controller init
         self.kondo = Rcb4BaseLib()
-        _uart = UART(1, 115200, parity=0)
+        _uart = UART(1, 115200, parity=0, timeout = 1000)
         self.kondo.open(_uart)
-        
+        print(self.kondo.checkAcknowledge())
+
         # loading motion dictionary
-        
+
         #self.motions = json.load("/motion/data_motion.json")
 
         self.motions = {
             "Soccer_WALK_FF" : {
                 "id"        : 8,
                 "time"      : (lambda c1 : 230 * c1 + 440),
-                "shift_x"   : (lambda c1, u1 : 4.3 / math.sin(u1 * 0.0140625 * math.pi / 180.) * 
+                "shift_x"   : (lambda c1, u1 : 4.3 / math.sin(u1 * 0.0140625 * math.pi / 180.) *
                                 math.sin(u1 * 0.028125 * c1 * math.pi / 180.) / 100.),
-                "shift_y"   : (lambda c1, u1 : -4.3 / math.sin(u1 * 0.0140625 * math.pi / 180.) + 
-                                4.3 / math.sin(u1 * 0.0140625 * math.pi / 180.) * 
+                "shift_y"   : (lambda c1, u1 : -4.3 / math.sin(u1 * 0.0140625 * math.pi / 180.) +
+                                4.3 / math.sin(u1 * 0.0140625 * math.pi / 180.) *
                                 math.cos(u1 * 0.028125 * c1 * math.pi / 180.) / 100.),
                 "shift_turn": (lambda u1 : u1 * 0.028125)
                 },
@@ -34,7 +36,7 @@ class Motion:
                 "time"      : (lambda c1 : 220 * c1 + 80),
                 "shift_x"   : 0,
                 "shift_y"   : 0,
-                "shift_turn": (lambda c1, u1 : u1 * 0.12 * c1) 
+                "shift_turn": (lambda c1, u1 : u1 * 0.12 * c1)
                 },
 
             "Soccer_Side_Step_Left" : {
@@ -60,7 +62,7 @@ class Motion:
                 "shift_y"   : (lambda c1: -3.3 * c1 / 100.),
                 "shift_turn": 0
                 },
-            
+
             "Soccer_Small_Step_Right" : {
                 "id"        : 12,
                 "time"      : (lambda c1 : 1250 * c1 + 150),
@@ -68,23 +70,23 @@ class Motion:
                 "shift_y"   : (lambda c1: 3.3 * c1 / 100.),
                 "shift_turn": 0
                 },
-            
+
             "Soccer_Take_Around_Left" : {
                 "id"        : 17,
                 "time"      : (lambda c1 : 1250 * c1 + 150),
-                "shift_x"   : (lambda c1 : 1.65 / math.sin(6.3 * math.pi / 180) * 
+                "shift_x"   : (lambda c1 : 1.65 / math.sin(6.3 * math.pi / 180) *
                                 (1 - math.cos(12.6 * c1 * math.pi / 180)) / 100.),
-                "shift_y"   : (lambda c1 : -1.65 / math.sin(6.3 * math.pi / 180) * 
+                "shift_y"   : (lambda c1 : -1.65 / math.sin(6.3 * math.pi / 180) *
                                 math.sin(12.6 * c1 * math.pi / 180) / 100.),
-                "shift_turn": (lambda c1 : -12.6 * c1) 
+                "shift_turn": (lambda c1 : -12.6 * c1)
                 },
 
             "Soccer_Take_Around_Right" : {
                 "id"        : 16,
                 "time"      : (lambda c1 : 1250 * c1 + 150),
-                "shift_x"   : (lambda c1 : 1.65 / math.sin(6.3 * math.pi / 180) * 
+                "shift_x"   : (lambda c1 : 1.65 / math.sin(6.3 * math.pi / 180) *
                                 (1 - math.cos(12.6 * c1 * math.pi / 180)) / 100.),
-                "shift_y"   : (lambda c1: 1.65 / math.sin(6.3 * math.pi / 180) * 
+                "shift_y"   : (lambda c1: 1.65 / math.sin(6.3 * math.pi / 180) *
                                 math.sin(12.6 * c1 * math.pi / 180) / 100.),
                 "shift_turn": (lambda c1 : 12.6 * c1)
                 },
@@ -104,7 +106,7 @@ class Motion:
                 "shift_y"   : None,
                 "shift_turn": None
                 },
-            
+
             "Soccer_HomePosition" : {
                 "id"        : 1,
                 "time"      : 1000
@@ -121,14 +123,15 @@ class Motion:
                 }
             }
 
-        
+
         # timer for motions
-        self._motion_duration = None
-        self._motion_start_time = None
-        
+        self._motion_duration = 0.0
+        self._motion_start_time = 0.0
+
         # head init
-        self.head_motion_states = json.load("/motion/head_motion.json")
-        self.head_enabled = False
+        with open("motion/head_motion.json", "r") as f:
+            self.head_motion_states = json.loads(f.read())
+        self.head_enabled = True
         self.head_yaw = 0
         self.head_pitch = 0
         self.head_state = 0
@@ -140,30 +143,27 @@ class Motion:
 
         # acceptable error in degrees
         self.angle_error_treshold = 5
-        
+
 
 ###########################################################################################
 # timer methods
-###########################################################################################        
-        
+###########################################################################################
+
     # False if controller is busy (playing motion).
     def _timer_permission_check(self):
-        if self._motion_duration is not None and self._motion_start_time is not None:
-            return self._motion_start_time + self._motion_duration <= time.ticks()
-        else:
-            return False
-            
+        return self._motion_start_time + self._motion_duration <= time.ticks()
+
     def _get_timer_duration(self, motion, args):
         return motion['time'](args['c1'])
-    
+
     def _set_timer(self, duration):
         self._motion_duration = duration
         self._motion_start_time = time.ticks()
-    
-                
+
+
 ###########################################################################################
 # several direct motion methods
-########################################################################################### 
+###########################################################################################
 
     # basic method of any motion appliance
     def do_motion(self, target_motion, args=None):
@@ -177,18 +177,18 @@ class Motion:
                     self.kondo.setUserParameter(1, args['u1'])
 
             self.kondo.motionPlay(self.current_motion['id'])
-        return target_motion['shift_x'], target_motion['shift_y'], 
+        return target_motion['shift_x'], target_motion['shift_y'],
 
 
-    # Free all servos (Almost similar to rhoban 'em' command)    
+    # Free all servos (Almost similar to rhoban 'em' command)
     def em(self):
         self.kondo.do_motion(self.motions['Free'])
-        
-        
+
+
     # home position of robot (rhoban init position)
     def init(self):
         self.do_motion(self.motions['Soccer_HomePosition'])
-        
+
     # ready position of robot (rhoban walk position)
     def ready(self):
         self.do_motion(self.motions['Soccer_Get_Ready'])
@@ -200,11 +200,13 @@ class Motion:
     def move_head(self):
         if self.head_enabled:
             if self._timer_permission_check():
+                print(self.head_motion_states)
                 self.head_state = (self.head_state + 1) % len(self.head_motion_states)
-                self.head_pitch = int(self.head_motion_states[self.head_state]['pitch'])
-                self.head_yaw = int(self.head_motion_states[self.head_state]['yaw'])
+                self.head_pitch = int(self.head_motion_states[str(self.head_state)]['pitch'])
+                self.head_yaw = int(self.head_motion_states[str(self.head_state)]['yaw'])
                 self.kondo.setUserParameter(20, self.head_pitch)
                 self.kondo.setUserParameter(19, self.head_yaw)
+                self._set_timer(500)
             else:
                 pass
         else:
@@ -215,7 +217,7 @@ class Motion:
 # hidden methods of motion control according to their arguments
 ###########################################################################################
 
-    # hardcode piece of Kefir code (don't do it like that next time) 
+    # hardcode piece of Kefir code (don't do it like that next time)
     def _get_turn_params(self, target, func):
         uo = -150
         co = 1
@@ -253,7 +255,7 @@ class Motion:
 
         if rotation_angle > self.angle_error_treshold:
             self.do_motion(self.motions['Soccer_Turn'], {'c1': c1, 'u1': u1})
-        
+
         distance = math.sqrt(x*x + y*y)
         if distance < self.max_blind_distance:
             step_num = distance // self.step_len
@@ -269,11 +271,11 @@ class Motion:
 
     def _lateral_control(self, lateral_args):
         pass
-        
+
 ###########################################################################################
-# 
+#
 ###########################################################################################
-        
+
     def apply(self, action):
         if self._timer_permission_check():
             if action['name'] == 'walk':
