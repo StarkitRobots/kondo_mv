d267a149bf2711a7bb37e1da1e05f469c3b92f88
Ilya <ris2012.r@yandex.ru>
2019-11-06 05:18:51 +0300

    fix main

--- main.py
+++ main.py
@@ -57,11 +57,6 @@ while(True):
 
         # motion part. Head movement.
         #motion.move_head()
-<<<<<<< HEAD
-        model.updateCameraPanTilt(motion.head_yaw * math.pi / 180., motion.head_pitch * math.pi / 180.)
-=======
-
->>>>>>> 280988b1d4785684fb35834ac83790085a67c75d
         # vision part. Taking picture.
         img=sensor.snapshot()
 
@@ -70,49 +65,6 @@ while(True):
         cameraDataRaw=vision.get(
             img, objects_list=["blue_posts", "ball", "white_posts_support"],#, "yellow_posts"],
             drawing_list=["blue_posts", "ball", "white_posts_support"])#, "yellow_posts"])
-
-        posts_num   = len (cameraDataRaw ["blue_posts"])
-        support_num = len (cameraDataRaw ["white_posts_support"])
-
-        left_post  = []
-        right_post = []
-
-        if (posts_num == 2):
-            post1 = cameraDataRaw ["blue_posts"] [0]
-            post2 = cameraDataRaw ["blue_posts"] [1]
-
-            if (post1.x () < post2.x ()):
-                left_post  = [post1]
-                right_post = [post2]
-
-            else:
-                left_post  = [post2]
-                right_post = [post1]
-
-            cameraDataRaw.update ({"left_blue_post"  : left_post})
-            cameraDataRaw.update ({"right_blue_post" : right_post})
-
-        elif (posts_num == 1):
-            post = cameraDataRaw ["blue_posts"] [0]
-
-            if (support_num == 1):
-                support = cameraDataRaw ["white_posts_support"] [0]
-
-                if (post.x() > support.x()):
-                    cameraDataRaw.update ({"left_blue_post"  : []})
-                    cameraDataRaw.update ({"right_blue_post" : [post]})
-                    print ("right post")
-
-                else:
-                    cameraDataRaw.update ({"left_blue_post"  : [post]})
-                    cameraDataRaw.update ({"right_blue_post" : []})
-                    print ("left post")
-
-            if (support_num == 0):
-                cameraDataRaw.update ({"left_blue_post"  : [post]})
-                cameraDataRaw.update ({"right_blue_post" : []})
-
-        cameraDataApproved = cameraDataRaw
 
         # model part. Mapping to world coords.
 
@@ -124,7 +76,6 @@ while(True):
                 selfPoints.append(
                     model.pic2r(observation[0] + observation[2]/2,
                     observation[1] + observation[3]))
-<<<<<<< HEAD
                 selfData[observationType].extend(selfPoints)
 
 
@@ -137,16 +88,5 @@ while(True):
     action = strat.generate_action(loc)
     print(action)
     print(loc.pf.token)
-=======
-            selfData[observationType] = selfPoints
-        #print(selfData)
-
-    #break
-
-    #loc.update(selfData)
-
-    action = strat.generate_action(loc)
-
->>>>>>> 280988b1d4785684fb35834ac83790085a67c75d
     #motion.apply(action)
 
