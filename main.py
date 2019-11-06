19c895462461eda4f9656e4739469525e6eb6430
Ilya <ris2012.r@yandex.ru>
2019-11-06 08:53:32 +0300

    fix main in collection of posts

--- main.py
+++ main.py
@@ -28,9 +28,9 @@ sensor.set_auto_whitebal(False)  # must be turned off for color tracking
 clock = time.clock()
 
 vision = Vision ({})
-vision.load_detectors ("vision/detectors_config.json")
+vision.load_detectors("vision/detectors_config.json")
 
-loc=Localization(-0.3, 0.9, 1.57)
+loc=Localization(0.0, 0.5, 1.57)
 strat=Strategy()
 motion=Motion()
 model=Model()
@@ -43,7 +43,7 @@ model.setParams(calib["cam_col"], robotHeight)
 model.updateCameraPanTilt(0, -3.1415/6)
 
 vision_postprocessing = Vision_postprocessing ()
-
+motion.move_head()
 t = 0
 # main loop
 while(True):
@@ -56,37 +56,44 @@ while(True):
     for i in range(motion.head_state_num):
 
         # motion part. Head movement.
-        #motion.move_head()
+        motion.move_head()
         # vision part. Taking picture.
         img=sensor.snapshot()
 
         #img.save ("kekb.jpg", quality=100)
 
         cameraDataRaw=vision.get(img, objects_list=
-        ["blue_posts", "ball", "white_posts_support"],	
-        drawing_list=["blue_posts", "ball", "white_posts_support"])		            
-        cameraDataProcessed = vision_postprocessing.process (cameraDataRaw, "blue_posts", "white_posts_support")		        
+        ["blue_posts", "ball", "white_posts_support"],
+        drawing_list=["blue_posts", "ball", "white_posts_support"])
+
+        cameraDataProcessed = vision_postprocessing.process (cameraDataRaw, "blue_posts", "white_posts_support")
+
         # model part. Mapping to world coords.
 
         # self means in robots coords
-        for observationType in cameraDataApproved:
-            selfData[observationType] = []
+        for observationType in cameraDataProcessed:
+            if observationType not in selfData.keys():
+                selfData[observationType] = []
             selfPoints = []
-            for observation in cameraDataApproved[observationType]:
+            for observation in cameraDataProcessed[observationType]:
                 selfPoints.append(
                     model.pic2r(observation[0] + observation[2]/2,
                     observation[1] + observation[3]))
-                selfData[observationType].extend(selfPoints)
-
+            selfData[observationType]+=selfPoints
 
         #print("keys = ", selfData.keys())
 
     #break
     loc.update(selfData)
-    loc.update_ball(selfData)
+    print("posts number = ", len(selfData["blue_posts"]))
     print("my_pose", loc.robot_position)
+    loc.update_ball(selfData)
+    print(loc.ballPosSelf)
+
     action = strat.generate_action(loc)
     print(action)
     print(loc.pf.token)
-    #motion.apply(action)
+    motion.apply(action)
+    break
+    time.sleep(10000)
 
