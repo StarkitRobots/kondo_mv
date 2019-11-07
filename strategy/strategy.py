8c0e031858a7b07933393b91ed25ae7b223429ad
YumeNikki <ienjoyeatingcorpses@gmail.com>
2019-11-07 15:28:18 +0300

    strategy and ball_approach fixes + angle reversion

--- strategy/strategy.py
+++ strategy/strategy.py
@@ -21,7 +21,7 @@ class Strategy:
         else:
             self.turn_counter = 0
 
-            x0, y0 = (1.8, 1.3)
+            x0, y0 = (0, 0)
             xr0, yr0, yaw = loc.robot_position
             xr = x0 - xr0
             yr = y0 - yr0
@@ -30,7 +30,7 @@ class Strategy:
                 return 0
             ang = math.acos(xr / dist)
 
-            if yr < 0:
+            if yr > 0:
                 ang = -ang
 
             return {"name" : "walk", "args" : (dist, ang - yaw)}
@@ -45,14 +45,14 @@ class Strategy:
         ang = math.acos(xb / dist)
         if dist > 0.3:
             if yb > 0:
-                return {"name" : "walk", "args" : (dist, ang)}
+                return {"name" : "walk", "args" : (dist, -ang)}
             else:
-                return {"name" : "walk", "args" : (dist, -1 * ang)}
+                return {"name" : "walk", "args" : (dist, ang)}
         elif ang < 1:
             if yb > 0:
-                return {"name" : "turn", "args" : (ang)}
+                return {"name" : "turn", "args" : (-ang)}
             else:
-                return {"name" : "turn", "args" : (-1 * ang)}
+                return {"name" : "turn", "args" : (ang)}
         else:
             return {"name" : "take_around_right", "args" : (1)}
 
@@ -62,7 +62,7 @@ class Strategy:
         xr, yr, yaw = loc.robot_position
         xb, yb = loc.ball_position
 
-        ba.get_data(xr, yr, xb, yb, yaw)
+        ba.get_data(xr, yr, xb, yb, -yaw)
 
         with open('data.json') as f:
             d = json.load(f)
@@ -78,20 +78,17 @@ class Strategy:
         if dist > dv[1]:
             ang = math.acos(rtraj[1][0] / dist)
             if rtraj[1][1] > 0:
-                return {"name" : "walk", "args" : (dist, ang)}
+                return {"name" : "walk", "args" : (dist, -ang)}
             else:
-                return {"name" : "walk", "args" : (dist, -1 * ang)}
+                return {"name" : "walk", "args" : (dist, ang)}
         elif dec == "turn left" or dec == "turn right":
             ang = math.acos(rtraj[1][0] / dist)
             if rtraj[1][1] > 0:
+                return {"name" : "turn", "args" : (-ang)}
+            else:
                 return {"name" : "turn", "args" : (ang)}
-            else:
-                return {"name" : "turn", "args" : (-1 * ang)}
         elif dec == "step left" or dec == "step right":
-            if rtraj[1][1] > 0:
-                return {"name" : "lateral_step", "args" : (rtraj[1][1])}
-            else:
-                return {"name" : "lateral_step", "args" : (-1 * rtraj[1][1])}
+            return {"name" : "lateral_step", "args" : (rtraj[1][1])}
         elif dec == "strike":
             return {"name" : "kick", "args" : (1)}
 
