1fb885496c615d231fc2859c4b35211cf95b7f3f
Khohklov Ivan <khohklov.iyu@gmail.com>
2020-03-31 00:41:50 +0300

    Sketch of lines impl in Particle class. Need to write get_self_coords_from_field and dist_particle2line func. Also added to-do list

--- localization/particle.py
+++ localization/particle.py
@@ -7,6 +7,11 @@ sys.path.append('../tools')
 from random import Random
 from robot import Robot
 
+#TODO : mb we should to calc landmark pos in self coord instead of calc 
+#       every observation in field coord. May work a little faster.
+#       
+#       mb move prob to Particle class variable
+
 class Particle(Robot):
     def observation_score(self, observations, landmarks, sense_noise):
         # particle weight calculation
@@ -19,15 +24,43 @@ class Particle(Robot):
                 if observations[color_landmarks]:
                     for observation in observations[color_landmarks]:
                         # calc posts coords in field for every mesurement
-                        x_posts = self.x + \
-                            observation[0] * math.cos(self.yaw) - \
-                            observation[1] * math.sin(self.yaw)
-                        y_posts = self.y + \
-                            observation[0] * math.sin(self.yaw) + \
-                            observation[1] * math.cos(self.yaw)
+                        x_posts, y_posts = get_field_coords_from_self(self, observation)
                         dist = math.sqrt(
                             (x_posts - landmark[0])**2 + (y_posts - landmark[1])**2)
                         dists.append(dist)
                 if (dists != []):
                     prob *= Random.gaussian(min(dists), sense_noise)
         return prob
+
+
+    def get_field_coords_from_self(self, observation):
+        x_field= self.x + observation[0] * math.cos(self.yaw) -
+                          observation[1] * math.sin(self.yaw)
+        y_field = self.y + observation[0] * math.sin(self.yaw) + 
+                           observation[1] * math.cos(self.yaw)
+        return (x_field, y_field)
+        
+    def get_self_coords_from_field(self, observation):
+        #implement func
+        return (x_self, y_self)
+
+    def lines_dist(self, l1, l2):
+        return dist_particle2line(l1, particle) - dist_particle2line(l2, particle)
+
+    def dist_particle2line(self, l):
+        #implement func 
+        return distance
+
+    def calc_lines_score(self, lines, landmarks):
+        '''
+        line = (ro, theta)
+        '''
+        prob = 1
+        dists = []
+        for landmark_line_field in landmarks["lines"]:
+            landmark_line_self = self.get_self_coords_from_field(landmark_line_field)
+            for line in lines:
+                dists.append(self.lines_dist(line, landmark_line_self))
+        if (dists != []):
+            prob *= Random.gaussian(min(dists), sense_noise)
+
