f7769f5ed4d0b78709e3b236145007f2fa8007bb
Ilya Ryakin <ryakin.is@phystech.edu>
2020-09-28 21:15:23 +0300

    Update __init__.py

--- src/__init__.py
+++ src/__init__.py
@@ -1,9 +1,10 @@
-# import vision
-# import model
-# import localization
-# import strategy
-# import motion
-# import odometry
-# import lowlevel
-# import logger
-# __all__ = ["vision", "localization", "strategy", "motion", "odometry", "lowlevel", "logger"]
+from .vision       import vision
+from .model        import model
+from .localization import localization
+from .strategy     import strategy
+from .motion       import motion
+from .odometry     import odometry
+from .lowlevel     import lowlevel
+from .logger       import logger
+
+__all__ = ["vision", "localization", "strategy", "motion", "odometry", "lowlevel", "logger"]
